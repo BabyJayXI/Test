b7bbd7fa5a2005a3ad0339031929d9d2ce9c108f
BabyJayXI <jaideniiwilliams@gmail.com>
2022-07-13 17:55:49 -0700

    added style practice

--- FlaskWedApp/Webapp.py
+++ FlaskWedApp/Webapp.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("JayWebApp")
 
 
 @app.route('/')
 def hello():
-    return 'Something Light'
+    return render_template('homepage.html')
+
+# This run my webapp and it starts it with my compatur as the server.
+app.run(host="0.0.0.0",port=5000,debug=True)
